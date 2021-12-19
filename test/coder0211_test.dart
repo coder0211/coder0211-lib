d1c4447b7effa2010793e11d4be0948c255ed561
hoand <nguyenduchoa.data@gmail.com>
2021-12-19 09:47:43 +0700

    update

--- test/coder0211_test.dart
+++ test/coder0211_test.dart
@@ -1,12 +1,12 @@
+import 'package:coder0211/coder0211.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:coder0211/coder0211.dart';
-
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
+  // test('adds one to input values', () {
+  //   final calculator = Calculator();
+  //   expect(calculator.addOne(2), 3);
+  //   expect(calculator.addOne(-7), -6);
+  //   expect(calculator.addOne(0), 1);
+  // });
 }
