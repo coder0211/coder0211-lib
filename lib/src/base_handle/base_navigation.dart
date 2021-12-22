d386b4a8873b5ee137aa9f0cff34679f962d3139
hoand <nguyenduchoa.data@gmail.com>
2021-12-22 20:01:37 +0700

    update

--- lib/src/base_handle/base_navigation.dart
+++ lib/src/base_handle/base_navigation.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class BaseNavigation {
   static push(BuildContext context,
       {required String routeName, Object? arguments, bool clearStack = false}) {
     if (clearStack)
-      Navigator.pushNamedAndRemoveUntil(context, '/$routeName', (r) => false,
+      Navigator.pushNamedAndRemoveUntil(context, '$routeName', (r) => false,
           arguments: arguments);
     else
       Navigator.pushNamed(context, '$routeName', arguments: arguments);
