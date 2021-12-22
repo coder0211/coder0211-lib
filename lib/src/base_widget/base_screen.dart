327d68417f3776cbd490b6d34f1c4afdedd2181f
hoand <nguyenduchoa.data@gmail.com>
2021-12-22 21:02:24 +0700

    update

--- lib/src/base_widget/base_screen.dart
+++ lib/src/base_widget/base_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 import '../../coder0211.dart';
@@ -7,29 +6,9 @@ abstract class BaseScreen extends StatefulWidget {
   const BaseScreen({Key? key}) : super(key: key);
 }
 
-abstract class BaseScreenState extends State<BaseScreen> {
+abstract class BaseScreenState<T extends BaseScreen> extends State<T> {
   bool _isConnected = false;
   bool isLoadDataSucceeded = false;
-  PreferredSizeWidget? appBar;
-  Color? backgroundColor, drawerScrimColor;
-  Widget? bottomNavigationBar,
-      bottomSheet,
-      drawer,
-      endDrawer,
-      floatingActionButton;
-  DragStartBehavior? drawerDragStartBehavior;
-  double? drawerEdgeDragWidth;
-  bool? drawerEnableOpenDragGesture,
-      endDrawerEnableOpenDragGesture,
-      extendBody,
-      extendBodyBehindAppBar,
-      primary,
-      resizeToAvoidBottomInset;
-  FloatingActionButtonAnimator? floatingActionButtonAnimator;
-  FloatingActionButtonLocation? floatingActionButtonLocation;
-  Function(bool)? onDrawerChanged, onEndDrawerChanged;
-  List<Widget>? persistentFooterButtons;
-  String? restorationId;
   @override
   void initState() {
     super.initState();
@@ -39,37 +18,11 @@ abstract class BaseScreenState extends State<BaseScreen> {
     BaseUtils.onWidgetBuildDone(_checkInit);
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appBar,
-      backgroundColor: backgroundColor,
-      bottomNavigationBar: bottomNavigationBar,
-      bottomSheet: bottomSheet,
-      drawer: drawer,
-      drawerDragStartBehavior:
-          drawerDragStartBehavior ?? DragStartBehavior.start,
-      drawerEdgeDragWidth: drawerEdgeDragWidth,
-      drawerEnableOpenDragGesture: drawerEnableOpenDragGesture ?? true,
-      drawerScrimColor: drawerScrimColor,
-      endDrawer: endDrawer,
-      endDrawerEnableOpenDragGesture: endDrawerEnableOpenDragGesture ?? true,
-      extendBody: extendBody ?? false,
-      extendBodyBehindAppBar: extendBodyBehindAppBar ?? false,
-      floatingActionButton: floatingActionButton,
-      floatingActionButtonAnimator: floatingActionButtonAnimator,
-      floatingActionButtonLocation: floatingActionButtonLocation,
-      onDrawerChanged: onDrawerChanged,
-      onEndDrawerChanged: onEndDrawerChanged,
-      persistentFooterButtons: persistentFooterButtons,
-      primary: primary ?? true,
-      resizeToAvoidBottomInset: resizeToAvoidBottomInset,
-      restorationId: restorationId,
-      body: SafeArea(
-        child: BaseViewLoading(
-          isLoading: !_isConnected || !isLoadDataSucceeded,
-          child: body(),
-        ),
+  Widget frame() {
+    return SafeArea(
+      child: BaseViewLoading(
+        isLoading: !_isConnected || !isLoadDataSucceeded,
+        child: body(),
       ),
     );
   }
