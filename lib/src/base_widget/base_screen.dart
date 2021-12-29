faeff9fae5a8f702f3ba9124909ce07c4706ca24
hoand <nguyenduchoa.data@gmail.com>
2021-12-29 22:27:33 +0700

    update code

--- lib/src/base_widget/base_screen.dart
+++ lib/src/base_widget/base_screen.dart
@@ -1,34 +1,19 @@
 import 'package:flutter/material.dart';
 
-import '../../coder0211.dart';
-
 abstract class BaseScreen extends StatefulWidget {
   const BaseScreen({Key? key}) : super(key: key);
 }
 
 abstract class BaseScreenState<T extends BaseScreen> extends State<T> {
-  bool _isConnected = false;
-  bool isLoadDataSucceeded = false;
   @override
   void initState() {
     super.initState();
     print('\≈≈≈≈≈≈≈≈≈≈≈≈ Current screen ≈≈≈≈≈≈≈≈≈≈≈≈> ' +
-        this.runtimeType.toString().replaceFirst('State', '') +
+        this
+            .runtimeType
+            .toString()
+            .replaceFirst('State', '')
+            .replaceAll('_', '') +
         '\n');
-    BaseUtils.onWidgetBuildDone(_checkInit);
-  }
-
-  Widget frame() {
-    return BaseViewLoading(
-      isLoading: !_isConnected || !isLoadDataSucceeded,
-      child: SafeArea(bottom: false, child: body()),
-    );
-  }
-
-  Widget body();
-
-  Future<void> _checkInit() async {
-    _isConnected = await BaseUtils.checkConnection();
-    setState(() {});
   }
 }
