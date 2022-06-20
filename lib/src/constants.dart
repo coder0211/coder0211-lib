516fd155783653aded8314a38ff826082feb6d32
hoand <nguyenduchoa.data@gmail.com>
2022-06-20 08:57:09 +0700

    update example

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,6 +1,5 @@
 class Constants {
   Constants._();
-  static String KEY_HTTP = 'http';
   static int ID_LENGTH = 11;
   static String REGEX_EMAIL =
       r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
